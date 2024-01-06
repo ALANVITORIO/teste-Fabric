a4ff13e69de928e53430b93048d958559eadcaa8
Alan Vitório <alanvitorio1985@gmail.com>
2024-01-06 19:29:19 -0300

    teste

--- script.js
+++ script.js
@@ -2,37 +2,27 @@ document.addEventListener('DOMContentLoaded', function () {
   const canvas = new fabric.Canvas('c');
   const canvas1 = new fabric.Canvas('c1');
 
-  // fruta
   fabric.Image.fromURL('fruta.webp', function (oImg) {
     oImg.scaleToWidth(200);
     oImg.scaleToHeight(200);
     oImg.set({ left: 200, top: 200, angle: 0 });
-    oImg.set({ transformMatrix: [1, 0, 0.5, 1, 0, 0] });
-
     canvas.add(oImg);
   });
 
-  // carro
   fabric.Image.fromURL('bmw.webp', function (oImg) {
     oImg.scaleToWidth(200);
     oImg.scaleToHeight(200);
     oImg.set({ left: 50, top: 50, angle: 0 });
-
     canvas.add(oImg);
   });
 
-  //computador
   fabric.Image.fromURL('pc.webp', function (oImg) {
     oImg.scaleToWidth(200);
     oImg.scaleToHeight(200);
     oImg.set({ left: 350, top: 50, angle: 0 });
-
     canvas.add(oImg);
   });
 
-  //outro canvas
-
-  // Criar o circulo com o texto no (c1)
   const text = new fabric.Text('hello world', {
     fontSize: 30,
     originX: 'center',
@@ -54,4 +44,39 @@ document.addEventListener('DOMContentLoaded', function () {
 
   canvas1.add(group);
   canvas1.renderAll();
+
+  var controlPoints = [
+    new fabric.Circle({
+      left: 200,
+      top: 200,
+      radius: 5,
+      fill: 'red',
+      hasControls: false,
+    }),
+    new fabric.Circle({
+      left: 400,
+      top: 200,
+      radius: 5,
+      fill: 'red',
+      hasControls: false,
+    }),
+    new fabric.Circle({
+      left: 200,
+      top: 400,
+      radius: 5,
+      fill: 'red',
+      hasControls: false,
+    }),
+    new fabric.Circle({
+      left: 400,
+      top: 400,
+      radius: 5,
+      fill: 'red',
+      hasControls: false,
+    }),
+  ];
+
+  controlPoints.forEach(function (point) {
+    canvas.add(point);
+  });
 });
